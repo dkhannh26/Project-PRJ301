52aec6da87851b13510501eab6c0064a09288882
dkhannh26 <duongquangkhanhlp@gmail.com>
2023-10-27 16:09:22 +0700

    init project

---
+++ project_prj301_group1/src/main/java/DAO/DAOaccount.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import connnectDB.connectDB;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import entity.account;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DAOaccount extends connectDB {
+
+    //------------account handele 
+    public List<account> getAll() {
+        List<account> listAccount = new ArrayList<>();
+        String sql = "select * from account";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                account a = new account(rs.getString("username"),
+                        rs.getString("password"), rs.getString("email"));
+                listAccount.add(a);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return listAccount;
+    }
+
+    public void insert(account a) {
+        String sql = "INSERT INTO [dbo].[account]\n"
+                + "           ([username]\n"
+                + "           ,[password]\n"
+                + "           ,[email])\n"
+                + "     VALUES\n"
+                + "           (?,?,?)";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, a.getUsername());
+            st.setString(2, a.getPassword());
+            st.setString(3, a.getEmail());
+            st.executeUpdate();
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    public account getAccountByUserName(String username) {
+
+        String sql = "select * from account where username=?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, username);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                 account a = new account(rs.getString("username"),
+                        rs.getString("password"), rs.getString("email"));
+                return a;
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        DAOaccount account = new DAOaccount();
+        List<account> listAccount = account.getAll();
+        System.out.println(listAccount.get(0).toString());
+
+
+    }
+}
---
+++ project_prj301_group1/src/main/java/entity/account.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class account {
+    String username;
+    String password;
+    String email;
+
+    public account(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "account{" + "username=" + username + ", password=" + password + ", email=" + email + '}';
+    }
+    
+    
+}
